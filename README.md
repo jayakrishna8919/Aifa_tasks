a252343ee0a616156bbacdef34955eae2073c60e
Jayakrishna <vesujakr@gmail.com>
2025-11-17 12:36:25 +0530

    updated

--- README.md
+++ README.md
@@ -9,4 +9,9 @@ Day -3
 -->In many_to_many.py ,made two models with many to many relationship
 -->asyncio_await_code.py is just an example of the async await python concept
 -->In connecting_postgres.py,given basic async await code in show_data function for better performance
--->
+day -4
+--> Create a FastAPI application
+-->Build REST endpoints for a to-do list application
+-->Implement request validation using Pydantic
+-->Handle different HTTP methods (GET, POST, PUT, DELETE)
+day -5
--- day 5 tasks/app/core/logger.py
+++ day 5 tasks/app/core/logger.py
@@ -2,18 +2,12 @@
 import logging
 
 logging.basicConfig(filename="newfile.log",filemode ='w+')
-
-
-
-
 logger = logging.getLogger("fastapi_project")
-
-
 logger.setLevel(logging.DEBUG)
 
-# ch = logging.StreamHandler()
-# ch.setLevel(logging.DEBUG)
-# formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-# ch.setFormatter(formatter)
-# logger.addHandler(ch)
-# print(ch)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(" %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+print(ch)
